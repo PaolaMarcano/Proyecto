7ac24e65c8a2704e534ebeacefc7a08c210fbf65
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2023-11-29 23:54:05 -0400

    Validar error vacío

--- routes/respuestas.js
+++ routes/respuestas.js
@@ -1,6 +1,7 @@
 function errores_res(err, resp) {
     if (err.codigo && err.mensaje && err.mensaje.sqlMessage) { resp.render('error', { message: err.mensaje.sqlMessage, error: { status: err.codigo } }) }
     else if (err.codigo && err.mensaje) { resp.render('error', { message: err.mensaje, error: { status: err.codigo } }) }
+    else if (Object.keys(err).length == 0) { resp.status(500).send("Ha ocurrido un error inesperado") }
     else { resp.status(500).send(err) }
 }
 
